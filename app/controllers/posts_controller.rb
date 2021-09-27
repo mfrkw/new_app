eda8df1f19e3ac9162758aadfc6762871a439457
mfrkw <xjdk3766@gmail.com>
2021-09-27 11:12:34 +0000

    [edit.add]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find_by(id: params[:id])
   end
-  
-  
+
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(content: params[:content])
     if @post.save
@@ -19,6 +19,21 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
     else
       render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
     end
   end
 end
